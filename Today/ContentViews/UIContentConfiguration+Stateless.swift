ecfaca8419d2bc0db990bca614cbb1b748c251b3
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-03 21:06:41 +0000

    Conform to the content view protocol

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Mary Moreira on 03/10/2022.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
