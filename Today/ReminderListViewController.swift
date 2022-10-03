0cfa7602b21d6d038ed1bc979eba33b0928aef58
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-03 15:32:10 +0000

    update list controller

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class ReminderListViewController: UIViewController {
-
+class ReminderListViewController: UICollectionViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
     }
-
-
+    
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+    
+    
 }
 
