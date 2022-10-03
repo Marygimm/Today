80d55deb5619d9b46eacb38f9a72d020b97d408e
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-03 21:37:37 +0000

    Filtering reminders
    Create a list style enumeration

---
+++ Today/Models/ReminderListStyle.swift
@@ -0,0 +1,25 @@
+//
+//  ReminderListStyle.swift
+//  Today
+//
+//  Created by Mary Moreira on 03/10/2022.
+//
+import Foundation
+
+enum ReminderListStyle: Int {
+    case today
+    case future
+    case all
+    
+    func shouldInclude(date: Date) -> Bool {
+        let isInToday = Locale.current.calendar.isDateInToday(date)
+        switch self {
+        case .today:
+            return isInToday
+        case .future:
+            return (date > Date.now) && !isInToday
+        case .all:
+            return true
+        }
+    }
+}
