e50b806182ae5da117115187766c6d9d2d7f8b8f
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-13 14:34:20 +0000

    Display errors to the user

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -133,5 +133,16 @@ class ReminderListViewController: UICollectionViewController {
         collectionView.backgroundView = backgroundView
     }
     
+    func showError(_ error: Error) {
+        let alertTitle = NSLocalizedString("Error", comment: "Error alert title")
+        let alert = UIAlertController(title: alertTitle, message: error.localizedDescription, preferredStyle: .alert)
+        let actionTitle = NSLocalizedString("OK", comment: "Alert OK button title")
+        alert.addAction(UIAlertAction(title: actionTitle, style: .default, handler: { [weak self] _ in
+            self?.dismiss(animated: true)
+        }))
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
